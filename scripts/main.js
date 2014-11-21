4cfcadc9030ea5af401ad7848fafbfe86b83afbe
Chris Mills <cmills@opera.com>
2014-11-21 22:25:08 +0000

    initial repo population

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc == 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+	} else {
+	  myImage.setAttribute ('src','images/firefox-icon.png');
+	}
+}
+
+// Personalized welcome message code
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
