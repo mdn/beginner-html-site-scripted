2081f293076c206cfae47613507a067241b0aec9
Chris Mills <cmills@opera.com>
2014-11-24 09:54:26 +0000

    change equality operator to identity

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if(mySrc == 'images/firefox-icon.png') {
+	if(mySrc === 'images/firefox-icon.png') {
       myImage.setAttribute ('src','images/firefox2.png');
 	} else {
 	  myImage.setAttribute ('src','images/firefox-icon.png');
