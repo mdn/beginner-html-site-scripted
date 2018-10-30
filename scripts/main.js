f10bf21ef67aa43b9479ef6e01354dd0dc925261
Prateekgupta9819 <36070161+Prateekgupta9819@users.noreply.github.com>
2018-10-30 12:51:22 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,26 @@
-// Image switcher code
-
 var myImage = document.querySelector('img');
-
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/firefox-icon.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
+      myImage.setAttribute ('src', ' images/firefox2.png');
 	} else {
-	  myImage.setAttribute ('src','images/firefox-icon.png');
+	  myImage.setAttribute ('src', ' images/firefox-icon.png');
 	}
 }
-
-// Personalized welcome message code
-
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+function setname() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
   myHeading.innerHTML = 'Mozilla is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')) {
-  setUserName();
+  setname();
 } else {
   var storedName = localStorage.getItem('name');
   myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
-
 myButton.onclick = function() {
-  setUserName();
+  setname();
 }
